ba89fda205ac8c5610e2ed2d10b1a521846f1461
Isaac <your.email@example.com>
2024-12-12 04:26:27 +0300

    Event Listeners Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
+// This function will be called when the button is clicked
+function clickAlert() {
+    alert('I was clicked!');
 }
+
+// This function sets up the event listener
+function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+}
+
+// Call the function to set up the event listener
+addingEventListener();
+
